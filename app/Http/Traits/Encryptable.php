e14a332a0f6b2f7c9f817adb811b3f9ebb2af88e
Corey Acri <acri.corey@gmail.com>
2018-04-12 12:44:56 -0400

    double check the trait function

--- app/Http/Traits/Encryptable.php
+++ app/Http/Traits/Encryptable.php
@@ -6,21 +6,26 @@ use Illuminate\Support\Facades\Crypt;
 
 trait Encryptable
 {
+ 
     public function getAttribute($key)
     {
         $value = parent::getAttribute($key);
-
-        if (in_array($key, $this->encryptable)) {
+ 
+        if (in_array($key, $this->encryptable) && ( ! is_null($value)))
+        {
             $value = Crypt::decrypt($value);
         }
+ 
+        return $value;
     }
-
+ 
     public function setAttribute($key, $value)
     {
-        if (in_array($key, $this->encryptable)) {
+        if (in_array($key, $this->encryptable))
+        {
             $value = Crypt::encrypt($value);
         }
-
+ 
         return parent::setAttribute($key, $value);
     }
-}
+} 
